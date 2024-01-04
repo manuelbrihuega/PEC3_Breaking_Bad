d81ec6c5d862d685fc68af4c9ee869f4d4d15bbc
Manuel Jesus de la Calle Brihuega <mdelacalleb@uoc.edu>
2024-01-04 14:05:45 +0100

    Añadida carga lazy a algunas imagenes e iframes

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -12,3 +12,22 @@ function cargarFraseCelebre() {
         fraseCelebre.innerText = frases[(Math.floor(Math.random() * frases.length))].frase;
     }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyBackgrounds = [].slice.call(document.querySelectorAll(".lazy-background"));
+  
+    if ("IntersectionObserver" in window) {
+      let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+            lazyBackgroundObserver.unobserve(entry.target);
+          }
+        });
+      });
+  
+      lazyBackgrounds.forEach(function(lazyBackground) {
+        lazyBackgroundObserver.observe(lazyBackground);
+      });
+    }
+});
